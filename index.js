de707e0288abdc381785607a2f635f0f3115a9af
moumen <moumen@gmail.com>
2024-02-07 12:11:43 +0100

    NEW HTML FILE

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const fs = require('node:fs')
-fs.readFile('readme.txt','utf-8',(err,data)=>{
-console.log(data)
-})
+const fs = require("node:fs");
+
+const myFileIndex = "index.html";
+
+fs.readFile("readme.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+
+fs.readFile("index.html", "utf8", (err, data) => {
+  console.log(data);
+});

82dec10af6f2d9d4a43c17955e39371103e50266
moumen <moumen@gmail.com>
2024-02-07 12:22:51 +0100

    nwe test

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const fs = require("node:fs");
 
 const myFileIndex = "index.html";
 
-fs.readFile("readme.txt", "utf-8", (err, data) => {
+/*fs.readFile("readme.txt", "utf-8", (err, data) => {
   console.log(data);
 });
 
 fs.readFile("index.html", "utf8", (err, data) => {
   console.log(data);
+});*/
+
+fs.writeFile("./readme.txt", "HELLO WORLD!!!!!!", () => {
+  console.log("nwe test");
 });
